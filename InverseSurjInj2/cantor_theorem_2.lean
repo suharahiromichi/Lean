b7c7879ccdd2ba899eac0472d265c662a15495af
SUHARA Hiromichi <suhara@mesa.local>
2025-05-16 22:00:24 +0900

    修正した。

--- InverseSurjInj2/cantor_theorem_2.lean
+++ InverseSurjInj2/cantor_theorem_2.lean
@@ -22,7 +22,7 @@ example (f : A → Set A) : ¬ Surjective f := by
   let B := {x | x ∉ f x}
 
   -- f は全射なので， f x = B となる x が存在する
-  -- 前提のexists ``∃ a, f a = B`` の場合分けをする。
+  -- 前提の H : exists ``∃ a, f a = B`` の場合分けをする。
   obtain ⟨x, hx⟩ := hsurj B
 --obtain H := hsurj B
 --rcases H with ⟨x, hx⟩
@@ -34,28 +34,33 @@ example (f : A → Set A) : ¬ Surjective f := by
     constructor
 
     -- 左から右を示す
+    -- ← 
     case mp =>
       -- x ∈ B だと仮定する
       intro hB
 
       -- B の定義から， x ∉ f x
+      -- replace は have と同じ、
+      -- ただし同名の補題 hB は上書きされて、無くなる。
       replace hB : x ∉ f x := by tauto
-
+  
       -- f x = B だったから， x ∉ B
+      -- rwa は、rw のあと assumption を行う。
       rwa [hx] at hB
 
     -- 右から左を示す
+    -- →
     case mpr =>
       -- x ∉ B だと仮定する
       intro hB
 
       -- f x = B だったから， x ∉ f x
+      -- 前提 hB は、置き換えられる。
       replace hB : x ∉ f x := by rwa [← hx] at hB
 
       -- B の定義から， x ∈ B
       assumption
   -- H1 の証明終わり。
-  
   -- H1 は矛盾である．
   tauto
 
